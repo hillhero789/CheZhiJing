745e4ff1096efafa65d501ed1d288247c2d0ad8d
hillhero789 <hillhero789@163.com>
2017-07-04 21:45:39 +0800

    add leancloud success
    add leancloud success

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as AV  from 'leancloud-storage';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +10,21 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    var APP_ID = 'jFJHb5ixb7XLc2tXMH5NfQuT-gzGzoHsz';
+    var APP_KEY = 'Ys2e9epw49uvfXKaUoGFyy0m';
 
+    AV.init({
+      appId: APP_ID,
+      appKey: APP_KEY
+    });
+
+    var TestObject = AV.Object.extend('TestObject');
+    var testObject = new TestObject();
+    testObject.save({
+      words: 'Hello World!'
+    }).then(function (object) {
+      console.log('rocks!')
+    })
   }
 
 }

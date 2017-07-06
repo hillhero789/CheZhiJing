a617fa33544d331245e6c752c75983066feaced9
hillhero789 <hillhero789@163.com>
2017-07-06 10:49:24 +0800

    adding slides etc.
    增加slides，在README中增加项目模块描述

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+项目包含系统：
+1、用户系统
+    1.1、实现注册登录功能，注册通过手机发送信息验证直接注册，注册后修改个人信息。
+    1.2、
+    1.3、
+2、订单系统
+    2.1、使用微信/支付宝支付
+    2.2、下单
+3、广告系统
+    3.1、前置页广告
+    3.2、首页滚动广告
+    3.3、
+4、推送系统
+5、定位系统
+
+
+
+
+
+
 This is a starter template for [Ionic](http://ionicframework.com/docs/) projects.
 
 ## How to use this template
@@ -23,4 +43,8 @@ $ ionic cordova run ios
 ```
 
 Substitute ios for android if not on a Mac.
+
+
+
+
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,30 +1,42 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import * as AV  from 'leancloud-storage';
+
+import { Slides } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+
+import * as AV from 'leancloud-storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage{
+  @ViewChild('mySlides') slidesForAd:Slides
   constructor(public navCtrl: NavController) {
-    var APP_ID = 'jFJHb5ixb7XLc2tXMH5NfQuT-gzGzoHsz';
-    var APP_KEY = 'Ys2e9epw49uvfXKaUoGFyy0m';
 
-    AV.init({
-      appId: APP_ID,
-      appKey: APP_KEY
-    });
-
-    var TestObject = AV.Object.extend('TestObject');
-    var testObject = new TestObject();
-    testObject.save({
-      words: 'Hello World!'
-    }).then(function (object) {
-      console.log('rocks!')
-    })
+    /* 
+     var APP_ID = 'jFJHb5ixb7XLc2tXMH5NfQuT-gzGzoHsz';
+     var APP_KEY = 'Ys2e9epw49uvfXKaUoGFyy0m';
+ 
+     AV.init({
+       appId: APP_ID,
+       appKey: APP_KEY
+     });
+ 
+     var TestObject = AV.Object.extend('TestObject');
+     var testObject = new TestObject();
+     testObject.save({
+       words: 'Hello World!'
+     }).then(function (object) {
+       console.log('rocks!')
+     })
+   */
+  }
+  
+  
+  onImgClick(){
+    console.log(this.slidesForAd.length());
   }
 
 }
